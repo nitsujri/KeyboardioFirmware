f4fab4ec3b198dc9a28802c6e7b50370ee504c50
Justin Louie <justin@guavapass.com>
2018-01-06 15:09:20 +0800

    Changing key layout to closer match my Diverge 3. Updating instructions

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ===
 Change Keys
 
-1. Run ./build_keys.sh
-1. Upload to Arduino Micro using IDE 1.0.5
+1. Download Arduino 1.0.5
+1. Rename it to Arduino_1.0.5
+1. Change something in layouts/qwerty*
+1. Run `make`
+1. Open using Arduino IDE
+1. Select Board: Arduino Micro
+1. Select Serial Port: tty.usb*
+1. Power cycle keyboard
+1. Press upload in first 30 seconds
--- keymaps.h
+++ keymaps.h
@@ -1,13 +1,13 @@
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/qwerty.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/qwerty_fn2.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/dvorak.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/dvorak_fn2.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/colemak.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/colemak_fn2.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/maltron.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/maltron_fn2.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/workman.h"
-#include "/Users/justinlouie/arduino/ArduinoKeyboard/layouts-generated/workman_fn2.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/qwerty.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/qwerty_fn2.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/dvorak.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/dvorak_fn2.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/colemak.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/colemak_fn2.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/maltron.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/maltron_fn2.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/workman.h"
+#include "/Users/justin/c/ArduinoKeyboard/layouts-generated/workman_fn2.h"
 #define LAYERS 8
 static const Key keymaps[LAYERS][ROWS][COLS] = {
 	KEYMAP_QWERTY
@@ -23,4 +23,3 @@ static const Key keymaps[LAYERS][ROWS][COLS] = {
 
 
 };
-
